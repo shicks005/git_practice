08ea911639073e818ed9049c4df5fbfe34d24fca
shicks005 <shicks0106@gmail.com>
2026-09-10 12:40:06 -0500

    Hello World!!!!!!

--- Documents/ cs250/PaintApp/src/paintapp/PaintApp.java
+++ Documents/ cs250/PaintApp/src/paintapp/PaintApp.java
@@ -41,7 +41,7 @@ public class PaintApp extends Application {
    */
     public static void main(String[] args) {
         //launch(args);
-        System.out.print("Hello Git");
+        System.out.print("Hello");
     }
     
 }
